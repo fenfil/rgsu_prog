653ef9e44b81162a09d5e9f1b4253bd47536b29e
fenfil <vladimir.zaykovskiy@ya.ru>
2020-11-09 10:44:15 +0300

    update chat

--- client/script.js
+++ client/script.js
@@ -1,6 +1,6 @@
-const socket = io("http://localhost:3000");
+const socket = io("/");
+
 const userInput = document.getElementById("username");
-const roomInput = document.getElementById("room");
 const startBtn = document.getElementById("start");
 const usersWrapper = document.getElementById("users");
 const messagesWrapper = document.getElementById("messages");
@@ -11,7 +11,6 @@ const page2 = document.getElementById("page-2");
 const roomTitle = document.getElementById("room-title");
 
 let name,
-  room,
   users = [];
 
 socket.on("connect", () => {
@@ -21,45 +20,31 @@ socket.on("connect", () => {
 startBtn.addEventListener("click", e => {
   e.preventDefault();
   name = userInput.value;
-  room = roomInput.value;
 
   page1.classList.toggle("hidden");
   page2.classList.toggle("hidden");
 
-  roomTitle.innerText = `Room: ${room}`;
-
   join();
 });
+
 msgBtn.addEventListener("click", e => {
   e.preventDefault();
   const text = msgInput.value;
   msgInput.value = "";
-  socket.emit("msg", text);
+  socket.emit("msg", text, text => {
+    if (text) {
+      messagesWrapper.innerHTML += msgNode({ text });
+    }
+  });
 });
 
-const userNode = user => `<span>${user}</span>`;
-const msgNode = msg => `<p>${msg.name}: ${msg.text}</p>`;
-const updateUsers = () =>
-  (usersWrapper.innerHTML = users.map(userNode).join(""));
+const msgNode = msg => `<p>${msg.name ? `${msg.name}: ` : ""}${msg.text}</p>`;
 
 function join() {
-  socket.emit("join", { room, name }, data => {
-    users = data.users;
-    messagesWrapper.innerHTML = data.messages.map(msgNode).join("");
-    updateUsers();
+  socket.emit("join", { name }, data => {
+    messagesWrapper.innerHTML = data.map(msgNode).join("");
   });
   socket.on("msg", data => {
     messagesWrapper.innerHTML += msgNode(data);
-  });
-  socket.on("user_joined", name => {
-    console.log("join", name);
-
-    users.push(name);
-    updateUsers();
-  });
-  socket.on("user_left", name => {
-    const i = users.indexOf(name);
-    if (i !== -1) users.splice(i, 1);
-    updateUsers();
   });
 }
--- server/server.js
+++ server/server.js
@@ -1,37 +1,40 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const rooms = {};
+const users = new Set();
+const messages = [];
 
 io.on("connect", socket => {
-  let room, name;
-  socket.on("join", ({ room: r, name: n }, cb) => {
-    room = r;
+  let name;
+  socket.on("join", ({ name: n }, cb) => {
     name = n;
-    if (rooms[room]) rooms[room].users.push(name);
-    else rooms[room] = { users: [name], messages: [] };
-
-    io.to(`room:${room}`).emit("user_joined", name);
-    socket.join(`room:${room}`);
-    cb(rooms[room]);
+    users.add(n);
+    cb(messages);
   });
-  socket.on("msg", text => {
-    rooms[room].messages.push({ text, name });
-    io.to(`room:${room}`).emit("msg", { text, name });
+  socket.on("msg", (text, cb) => {
+    if (text.startsWith("/")) {
+      if (text == "/room") {
+        return cb(Array.from(users));
+      }
+      return cb("Unknown command");
+    }
+    messages.push({ text, name });
+    io.emit("msg", { text, name });
   });
   socket.on("disconnect", () => {
-    if (!rooms[room]) return;
-
-    const i = rooms[room].users.indexOf(name);
-    rooms[room].users.splice(i, 1);
-    io.to(`room:${room}`).emit("user_left", name);
+    users.delete(name);
   });
 });
+
+app.use(morgan("tiny"));
+app.use(express.static(path.resolve(__dirname, "..", "client")));
 
 server.listen(3000, () => {
   console.log("Listening on 3000");
